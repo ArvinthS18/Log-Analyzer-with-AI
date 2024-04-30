2fab7224ed71886a54e4e242873757efa3c0fcdd
ArvinthS18 <aravindansubrmani2001@gmail.com>
2024-04-30 13:34:10 +0530

    Error Classification using api

--- frontend/test2/src/app/app.module.ts
+++ frontend/test2/src/app/app.module.ts
@@ -1,4 +1,45 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { HttpClientModule } from '@angular/common/http'; // Added HttpClientModule import
 
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule // Added HttpClientModule to imports
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { HttpClientModule } from '@angular/common/http'; // Added HttpClientModule import
+// import { FormsModule } from '@angular/forms'; // Added FormsModule import for ngModel
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule, // Added HttpClientModule to imports
+//     FormsModule // Added FormsModule to imports for ngModel
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
